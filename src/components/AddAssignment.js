8305850f0efff05e5f2077237eb14b252077896c
kate <katesawtell99@gmail.com>
2023-09-28 14:27:01 -0700

    AddAssignment improvemnt

--- src/components/AddAssignment.js
+++ src/components/AddAssignment.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { SERVER_URL } from '../constants'; 
+import { Link } from 'react-router-dom';
+
 
 function AddAssignment(props) {
   const [assignment, setAssignment] = useState({
-  name: '',
-  dueDate: '',
-  course:'',
-  courseId: 0,
+    assignmentName: '',
+    dueDate: '',
+    courseTitle: '',
+    courseId: 0,
   });
 
   const saveAssignment = (e) => {
@@ -14,12 +16,13 @@ function AddAssignment(props) {
     console.log("Assignment.save");
     // Handle the submission, e.g., send the assignment data to an API or update a state variable.
       try {
-        fetch(`${SERVER_URL}/assignment` ,
+        response = fetch(`${SERVER_URL}/assignment` ,
         {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(assignment)
         } )
+
         // Handle the response as needed (e.g., show a success message, redirect, etc.)
         console.log('Assignment added with ID:', response.data);
         alert('Assignment added successfully.');
@@ -51,40 +54,42 @@ function AddAssignment(props) {
     <div>
       <h3>Add New Assignment</h3>
       <button>
-      <Link to={`/`} >Home Page</Link>
+        <Link to={`/`}>Home Page</Link>
       </button>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Name:</label>
-        <input
-        type="text"
-        name="assignmentName"
-        value={assignment.assignmentName}
-        onChange={onChangeInput}
-        />
-      </div>
-      <div>
-        <label>Due Date:</label>
-        <input
-        type="date"
-        name="dueDate"
-        value={assignment.dueDate}
-        onChange={onChangeInput}
-        />
-      </div>
-      <div>
-        <label>Course:</label>
-        <textarea
-        type="text"
-        name="courseTitle"
-        value={assignment.courseTitle}
-        onChange={onChangeInput}
-        />
-      </div>
-        <button id="add" type="button" margin="auto" onClick={saveAssignment}>Add Assignments</button>
-    </form>
+      <form onSubmit={saveAssignment}>
+        <div>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="assignmentName"
+            value={assignment.assignmentName}
+            onChange={onChangeInput}
+          />
+        </div>
+        <div>
+          <label>Due Date:</label>
+          <input
+            type="date"
+            name="dueDate"
+            value={assignment.dueDate}
+            onChange={onChangeInput}
+          />
+        </div>
+        <div>
+          <label>Course:</label>
+          <textarea
+            type="text"
+            name="courseTitle"
+            value={assignment.courseTitle}
+            onChange={onChangeInput}
+          />
+        </div>
+        <button type="submit" id="add" style={{ margin: "auto" }}>
+          Add Assignment
+        </button>
+      </form>
     </div>
-    );
+  );
 }
 
 
